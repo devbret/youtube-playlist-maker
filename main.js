5f43d3a1bb38c214253381338d3e49e832d8e1b7
Bret Bernhoft <73839896+devbret@users.noreply.github.com>
2021-02-21 13:57:45 -0800

    Added new utility functions and logic.

--- main.js
+++ main.js
@@ -10,12 +10,26 @@ function main() {
     const displayList = document.querySelectorAll(`.display-playlist`)[0];
     //Support functions.
     function sortingURLs(z) {
-        return z;
+        const totalURLs = z.split(`,`)
+        //Add logic for filtering through URLs here.
+        return totalURLs;
+    }
+    function addToPlaylist(z) {
+        const playlistItem = document.createElement(`div`);
+        playlistItem.classList.add(`playlistItem`);
+        playlistItem.style.backgroundImage = `url(http://img.youtube.com/vi/${z}/0.jpg)`;
+        displayList.appendChild(playlistItem);
+    }
+    function displayURL(z) {
+        live.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${z}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     }
     //Primary event listeners.
     submit.addEventListener(`click`, function(){
         const videoID = sortingURLs(input.value);
-        live.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoID}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+        for (let i = 0; i < videoID.length; i++) {
+            addToPlaylist(videoID[i]);
+        }
+        displayURL(videoID);
     });
 }
 main();
